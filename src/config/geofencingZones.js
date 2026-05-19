edf61fb69d50ff082d63f75681c45c29ade8af33
e-nihilus <epardorayo@gmail.com>
2026-05-19 18:08:21 +0200

    new location

--- src/config/geofencingZones.js
+++ src/config/geofencingZones.js
@@ -1,8 +1,8 @@
 export const GEOFENCING_ZONES = [
   {
     id: 'barcelona-gaudi',
-    lat: 41.38715400593564,
-    lon: 2.170048777417063,
+    lat: 41.389250,
+    lon: 2.128806,
     radius: 100,
   },
 ]
